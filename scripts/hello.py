899b39bd9813e0337b2a88a89f3da4fae9a289d5
Matthew J. Milner <matterhorn103@proton.me>
2025-01-23 16:41:35 +0100

    Bare bones

---
+++ scripts/hello.py
@@ -0,0 +1,21 @@
+# /// script
+# requires-python = ">=3.11"
+# dependencies = [
+#   "platformdirs>=4.2.0",
+# ]
+# 
+# [tool.avogadro.script]
+# type = "energy"
+# display-name = "Yell Hello"
+# menu-path = [ "Extensions", 900 ]
+# 
+# [tool.avogadro.pixi]
+# channels = ["conda-forge"]
+#
+# [tool.avogadro.pixi.dependencies]
+# numpy = ">=2.2.0"
+# ///
+
+import numpy as np
+
+print("HELLO, WORLD!")
