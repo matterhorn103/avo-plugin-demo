e5199612d418d5e012ee49a164c138557a3809b7
Matthew Milner <matterhorn103@proton.me>
2025-11-15 00:26:58 +0100

    Revise pyproject.toml

--- example-scripts/hello.py
+++ example-scripts/hello.py
@@ -1,19 +1,13 @@
 # /// script
 # requires-python = ">=3.11"
 # dependencies = [
-#   "platformdirs>=4.2.0",
 # ]
 # 
 # [tool.avogadro.script]
-# type = "energy"
+# type = "command"
 # display-name = "Yell Hello"
 # menu-path = [ "Extensions", 900 ]
 # 
-# [tool.pixi]
-# channels = ["conda-forge"]
-#
-# [tool.pixi.dependencies]
-# numpy = ">=2.2.0"
 # ///
 
 import numpy as np
